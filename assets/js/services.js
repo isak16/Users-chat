0dff1d3275accacfd69b6910c19ae0c00d544c61
jakobjohansson <jakobjohansson2@icloud.com>
2017-03-28 14:30:15 +0200

    fix typo in service

--- assets/js/services.js
+++ assets/js/services.js
@@ -7,20 +7,20 @@ app.factory('api', function($http) {
     var url = "";
     return {
         get: function(type, id = null) {
-            url += type;
+            url += "/" + type;
 
             switch (type) {
                 case 'profile':
                     var profileId = (id != null) ? id : localStorage.userId;
-                    return $http.get(url + "/" + type + "/" + profileId);
+                    return $http.get(url + "/" + profileId);
                 break;
 
                 case 'messages':
-                    return $http.get(url + "/" + type + "/" + id);
+                    return $http.get(url + "/" + id);
                 break;
 
                 case 'sidebar':
-                    return $http.get(url + "/" + type + "/" + localStorage.userId);
+                    return $http.get(url + "/" + localStorage.userId);
                 break;
             }
         },
