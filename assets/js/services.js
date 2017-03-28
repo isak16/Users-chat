99a55d8f087e61051b3cc9d5d6bfde9c03a01c98
jakobjohansson <jakobjohansson2@icloud.com>
2017-03-28 15:41:59 +0200

    chats test case in api

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,33 +1,243 @@
-/**
- * Service for API requests
- * @type {String} Type of requests
- * Usage: api.get("sidebar");
- */
-app.factory('api', function() {
-    var profiles = [];
-    var chats = [];
+app.value('profiles', []);
+app.value('chats',
+    [
+        {
+            "group_id": "6e1072d3-dcb3-401b-8532-6061e11e4574",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2015-08-26T07:10:02 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "73f22d70-72ff-424c-8330-f92280c617e9",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2016-04-14T04:04:52 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "ce30ce7d-b968-4a69-bdfe-602d92bb8cfe",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2014-04-24T05:07:55 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "7895823e-b1cf-4516-9717-3248fb842bd4",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2016-01-26T09:31:58 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "8afe0449-4ecb-4f4b-a9d7-e1d07cf9495d",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2015-01-01T05:47:04 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "ffab4144-4d91-4c91-9588-65c681e26635",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2016-11-14T08:44:56 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "b4416849-1813-4273-90ef-07216d1a0cb0",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2014-10-18T01:34:39 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "cc6da9f1-b9f1-4276-af3e-772d4571e81c",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2014-10-01T04:29:26 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "3a6a84b9-5e9e-4b2f-be3f-25fd9f39fdaa",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2017-01-24T12:02:59 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "8a9a083e-d42a-4254-b263-2539c1fdee9b",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2016-05-15T01:20:53 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "9ac1d81b-ba87-4166-bb2c-e0c9d0713b0b",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2014-06-30T05:22:52 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "9c68d436-0477-45ed-a4a6-c0f9f9d4142c",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2017-01-13T11:38:16 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "e8e2436b-06a5-4855-b124-24d1c3472417",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2017-03-25T04:04:06 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "0239403a-9ba1-42cc-b4e1-1b74daeef587",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2016-02-12T07:52:08 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "27089609-5576-455b-a17f-5ca1891405e3",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2015-07-29T04:11:43 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "82bf2615-7abe-47e3-860a-292c23c0f63d",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2015-04-17T02:41:29 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "1d1fc252-bbdc-4f28-8d2e-ed7fad69b1a8",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2015-11-17T05:08:22 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "ac1eb0dc-c5bc-433f-867a-15cde1db2119",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2015-09-14T08:55:49 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "160b6b4d-681d-4e35-863a-37278c2a5e68",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2014-08-28T02:35:09 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "03862c3f-8dd0-4e2c-a281-9a37ea83cc20",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2016-12-09T06:15:47 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "e3c27cff-7538-42b7-a20f-f77465d8a50b",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2017-01-15T02:44:04 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "7640bb0f-9e54-42e8-84a8-20e708aa7972",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2015-08-31T08:44:26 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "263e4467-91c4-41ca-958a-bd1696ebfebf",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2014-11-12T02:21:55 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "d3266a36-9bf3-449d-8c6d-4797462ffcf8",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2016-08-06T02:28:33 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "367c5b41-55e1-464f-9fac-b8d08a3a027b",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2015-08-08T03:12:03 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "dfd70a46-d857-4db8-804a-36880f8f9589",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2014-08-11T09:32:46 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "0e920c48-3826-4320-814a-abc0df18ab46",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2016-10-09T11:09:49 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "b508456c-0416-413f-aebc-4a538a9b962a",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2015-07-19T10:31:08 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "dd300954-c2a2-463a-b004-e64ce7cb9368",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2014-01-01T12:58:45 -01:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        },
+        {
+            "group_id": "6b765d86-8402-4326-a83d-fd521c871714",
+            "group_name": "",
+            "picture": "http://www.funnyordie.com/thumbnails/177ae91679/fullsize",
+            "last_message_timestamp": "2016-06-16T08:21:05 -02:00",
+            "messages": "[{from:3366, message: \"HEj nuub\"}, {from:6666, message: \"Lel du kan vara nuub\"}]"
+        }
+    ]
+);
 
+app.factory('api', function(profiles, chats) {
     return {
-        get: function(type, id = null) {
+        get: function(type, id) {
             switch (type) {
                 case 'profile':
-                    var profileId = (id != null) ? id : localStorage.userId;
-                    return profiles[profileId];
+                    return profiles[id];
                 break;
 
                 case 'messages':
-                for (i = 0; i < chats.length; i++) {
-                    if (chats[i].id === id) {
-                        return chats[i];
+                    for (i = 0; i < chats.length; i++) {
+                        if (chats[i].group_id === id) {
+                            return chats[i];
+                        }
                     }
-                }
                 break;
 
                 case 'sidebar':
                     var temp = [];
+                    var profileChats = this.get("profile", id).group_chats;
                     for (var i = 0; i < chats.length; i++) {
-                        if (chats[i].id === id) {
-                            temp.push(chats[i]);
+                        for (var j = 0; j < profileChats.length; j++) {
+                            if (chats[i].group_id === profileChats[j]) {
+                                temp.push(chats[i]);
+                            }
                         }
                     }
                     return temp;
@@ -41,5 +251,5 @@ app.factory('api', function() {
                 }
             }
         }
-    }
+    };
 });
