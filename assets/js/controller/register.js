f931b835a07bfbe1af76c534304e252a79dbf3b5
Erik Tedfelt Lennström <erik.tedfelt.lennstrom@gmail.com>
2017-03-30 15:19:57 +0200

    Installd mongoose, express, body-parser

--- assets/js/controller/register.js
+++ assets/js/controller/register.js
@@ -3,10 +3,34 @@ app.run(function($rootScope) {
     $rootScope.users = [];
 });
 
-app.controller('register', function($scope){
+app.controller('register', function($scope, api, users){
     var user = {};
-    $scope.register = function(user){
-        console.log('hej');
-        console.log(user);
+    $scope.register = function(userReg){
+        api.users('add', userReg);
+
+    };
+    $scope.login = function(userLogin){
+
     };
 });
+
+var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+
+app.directive("compareTo", compareTo);
