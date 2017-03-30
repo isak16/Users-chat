ab73839e8065bc7fc56a1931b7d16c0f3328b8df
jakobjohansson <jakobjohansson2@icloud.com>
2017-03-30 20:13:20 +0200

    forgot $http

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,32 +1,32 @@
-app.factory('api', function() {
+app.factory('api', function($http) {
+    var url = "http://localhost:3000/";
     return {
-        url: "http://localhost:3000/",
         users: {
             get: function(id) {
-                return $http.get(this.url + "users/" + id);
+                return $http.get(  url + "users/" + id);
             },
             add: function(user) {
-                return $http.post(this.url + "users/", user);
+                return $http.post(  url + "users/", user);
             },
             remove: function(user) {
-                return $http.delete(this.url + "users/", user);
+                return $http.delete(  url + "users/", user);
             },
             update: function(user) {
-                return $http.put(this.url + "users/", user);
+                return $http.put(  url + "users/", user);
             },
             login: function(username, password) {
-                return $http.post(this.url + "users/login/", { "username": username, "password": password});
+                return $http.post(  url + "users/login/", { "username": username, "password": password});
             }
         },
         groups: {
             get: function(id) {
-                return $http.get(this.url + "groups/" + id);
+                return $http.get(  url + "groups/" + id);
             },
             add: function(group) {
-                return $http.post(this.url + "groups/", group);
+                return $http.post(  url + "groups/", group);
             },
             remove: function(id) {
-                return $http.delete(this.url + "groups/", group);
+                return $http.delete(  url + "groups/", group);
             }
         }
     };
