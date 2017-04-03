0069c96793a47e898f25de95431cb489835b7641
Erik Tedfelt Lennström <erik.tedfelt.lennstrom@gmail.com>
2017-04-03 13:43:00 +0200

    Error message inavlid email

--- assets/js/controller/register.js
+++ assets/js/controller/register.js
@@ -5,12 +5,11 @@ app.run(function($rootScope) {
 app.controller('register', function($scope, api){
     var user = {};
     $scope.register = function(userReg){
-        api.users('add', userReg);
-
+        console.log(userReg);
     };
 
     $scope.login = function(userLogin){
-
+        console.log(userLogin);
     };
     //$scope.userRegex = /^[a-zA-Z0-9]{1,}$/.test($scope.registerForm.username);
     $scope.loguser = function(){
