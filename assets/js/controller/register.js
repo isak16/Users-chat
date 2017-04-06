c28d863325342008a5be3464e51c334bd289d632
jakobjohansson <jakobjohansson2@icloud.com>
2017-04-06 11:49:04 +0200

    fix function

--- assets/js/controller/register.js
+++ assets/js/controller/register.js
@@ -5,15 +5,12 @@ app.run(function($rootScope) {
 app.controller('register', function($scope, api){
     var user = {};
     $scope.register = function(userReg){
-        if (userReg.password != userReg.confirmPassword) {
-            console.log("wrong passwords");
-            return false;
-        } else {
+
             delete userReg.confirmPassword;
             api.users.add(userReg).then(function (response) {
                 console.log("added user: " + response);
             });
-        }
+
     };
 
     $scope.login = function(userLogin){
