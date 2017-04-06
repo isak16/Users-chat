22b49e4bf1d97fe4cf5d9c96f3da817a0fe60b5c
Rebecka <rebecka.pylsy@gmail.com>
2017-04-06 07:29:45 +0200

    Added set status

--- assets/js/controller/sidebar.js
+++ assets/js/controller/sidebar.js
@@ -13,3 +13,13 @@ app.controller('newsSidebar', function ($scope) {
         {name: '@IsakProvideIT', image: 'assets/img/jobs.png'}
     ]
 });
+
+function setOffline() {
+    document.getElementById("yourStatus").setAttribute("class", "offline");
+}
+function setBusy() {
+    document.getElementById("yourStatus").setAttribute("class", "busy");
+}
+function setOnline() {
+    document.getElementById("yourStatus").setAttribute("class", "online");
+}
