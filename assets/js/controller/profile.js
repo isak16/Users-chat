e4b0615c757af05440ac7b1bbf7b99322595d482
Erik Tedfelt Lennström <erik.tedfelt.lennstrom@gmail.com>
2017-04-20 13:11:33 +0200

    Reset settings form when not saving

--- assets/js/controller/profile.js
+++ assets/js/controller/profile.js
@@ -1,15 +1,11 @@
 app.controller('settingsProfile' , function ($scope, $document, $sessionStorage, api) {
     $scope.$storage = $sessionStorage;
-    $scope.imgSrc = 'assets/img/jobs.png';
-    $scope.showName = 'Visningsnamn';
-    $scope.userName = 'Username';
-    $scope.yourEmail = 'namn.efternamn@exempel.com';
-    $scope.yourInfo = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada, est vitae commodo dapibus, ipsum velit euismod nibh, sit amet efficitur justo est ac augue.';
-    console.log($scope.lightTheme);
-    $scope.test = function () {
 
-
-    };
+    $scope.resetSettings = function() {
+        api.users.get($scope.$storage.user._id).then(function(response){
+            $scope.$storage.user = response.data;
+        });
+    }
 
     $scope.updateSettings = function() {
         api.users.update().then(function(response) {
